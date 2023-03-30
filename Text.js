a82d2caca2cdac7be6d9bacf8d596f7fc2910c24
alifnami1999 <alifnami1999@gmail.com>
2023-03-30 16:01:31 +0800

    Text.js

---
+++ Text.js
@@ -0,0 +1,47 @@
+let message ="BENR"
+console.log(message)
+console.log(typeof message)
+
+message = 123
+console.log(message,typeof message)
+
+
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num 
+//console.log(total, typeof total)
+
+let data = ["BENR",3523,true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name:"SOO",
+    age:50,
+    faculty:"FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+
+
+let messages = 
+[
+ {
+    name: "amri",
+    date: "1/4/2023",
+    messages: "Very Good Video",
+    like: 4000
+ },
+
+ {
+    name: "mael",
+    date: "1/4/2023",
+    messages: "Very Nice Video",
+    like: 4000
+ },
+]
+console.log(messages)
